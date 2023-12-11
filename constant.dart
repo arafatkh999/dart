baccc838fa575ae64ccb5472655445f357c9b496
arafatkh999 <arafatkh999@gmail.com>
2023-12-11 17:54:44 +0600

    operators-constant-if-else

---
+++ constant.dart
@@ -0,0 +1,18 @@
+void main (){
+  int randomNumber = 45;
+  randomNumber = 99;
+  print(randomNumber);
+
+
+  //Constant
+  //const   //Compile time constant
+  const pi = 3.1416;
+  print(pi);
+
+
+  //final  //Runtime constant
+  final String name ;
+  name = 'arafat';
+  print(name);
+
+}
---
+++ control_statement_if_else.dart
@@ -0,0 +1,10 @@
+void main (){
+  int time = 9;
+  if (time == 10){
+    print('Then tunnel open');
+  }
+  else {
+    print('Warning');
+  }
+  print('I will go home');
+}
